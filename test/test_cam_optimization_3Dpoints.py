94b2524876be9ea55064ec37c0bab5a04a4c85be
Miguel Armando Riem Oliveira <m.riem.oliveira@gmail.com>
2020-01-20 12:36:03 +0000

    Beatifying the objective function.

--- test/test_cam_optimization_3Dpoints.py
+++ test/test_cam_optimization_3Dpoints.py
@@ -5,14 +5,15 @@ import cv2
 import glob
 from scipy import optimize
 
+
 def generate_chessboard(size, dimensions=(9, 6)):
     objp = np.zeros((dimensions[0] * dimensions[1], 3), np.float32)
     objp[:, :2] = np.mgrid[0:dimensions[0], 0:dimensions[1]].T.reshape(-1, 2)
     objp = objp * size
     return objp
 
-def objective_function(params):
 
+def objective_function(params):
     rvec_left, tvec_left = find_cam_chess_realpoints('./fotos_calibration/top_left_camera_4.jpg', 0)
     rvec_right, tvec_right = find_cam_chess_realpoints('./fotos_calibration/top_right_camera_4.jpg', 1)
 
@@ -30,7 +31,7 @@ def objective_function(params):
     r_cam2tochess_vector, _ = cv2.Rodrigues(T2_chess_real[0:3, 0:3])
     # t_cam2tochess = np.zeros((3, 1))
     t_cam2tochess = T2_chess_real[0:3, 3]
-    imgpoints_right_optimize = cv2.projectPoints(objp, r_cam2tochess_vector, t_cam2tochess, k_right,
+    imgpoints_right_optimize = cv2.projectPoints(pts_chessboard, r_cam2tochess_vector, t_cam2tochess, k_right,
                                                  dist_right)
 
     img1 = cv2.imread('./fotos_calibration/top_right_camera_4.jpg')
@@ -48,20 +49,82 @@ def objective_function(params):
 
     # projection_matrix_right = np.matmul(k_right,T2_chess)
     objp_right = np.zeros((4, dimensions[0] * dimensions[1]), np.float32)
-    objp_right[0:3, :] = objp.T
+    objp_right[0:3, :] = pts_chessboard.T
     objp_right[3, :] = 1
 
     residuals = np.zeros((dimensions[0] * dimensions[1]))
 
     for i in range(dimensions[0] * dimensions[1]):
-        points_op = np.matmul(np.array(t2_chess), objp_right[:,i].reshape((4,1)))
-        points_real_right = np.matmul(t2_real, objp_right[:,i].reshape((4,1)))
-        residuals[i] = np.sqrt((points_op[0] - points_real_right[0]) ** 2 + (points_op[1] - points_real_right[1]) ** 2 + (points_op[2] - points_real_right[2]) ** 2)
+        points_op = np.matmul(np.array(t2_chess), objp_right[:, i].reshape((4, 1)))
+        points_real_right = np.matmul(t2_real, objp_right[:, i].reshape((4, 1)))
+        residuals[i] = np.sqrt(
+            (points_op[0] - points_real_right[0]) ** 2 + (points_op[1] - points_real_right[1]) ** 2 + (
+                        points_op[2] - points_real_right[2]) ** 2)
 
     return residuals
 
-def find_cam_chess_realpoints(fname,left_or_right):
 
+def objective_functionv2(params, points_chessboard):
+    # TODO you should read the image once and store in memory
+
+
+    # Get T from left camera to chess
+    rvec_cam_left, tvec_cam_left = find_cam_chess_realpoints('./fotos_calibration/top_left_camera_4.jpg', 0)
+    left_cam_T_chess = np.zeros((4, 4), np.float32)
+    left_cam_T_chess[0:3, 0:3], _ = cv2.Rodrigues(rvec_cam_left)
+    left_cam_T_chess[0:3, 3] = tvec_cam_left.T
+    left_cam_T_chess[3, :] = [0, 0, 0, 1]  # homogenize
+
+    # Get T from left cam to right cam (based on the params being oiptimized)
+    right_cam_T_left_cam = np.zeros((4, 4), np.float32)
+    right_cam_T_left_cam[0:3, 0:3], _ = cv2.Rodrigues(params[0:3])
+    right_cam_T_left_cam[0:3, 3] = params[3:].T
+    right_cam_T_left_cam[3, :] = [0, 0, 0, 1]  # homogenize
+
+    # Get aggregate T from cam_right to chess (optimized)
+    right_cam_T_chess_opt = np.matmul(right_cam_T_left_cam, left_cam_T_chess)
+
+    # Get T from right camera to chess (ground truth)
+    rvec_cam_right, tvec_cam_right = find_cam_chess_realpoints('./fotos_calibration/top_right_camera_4.jpg', 1)
+    right_cam_T_chess_ground_truth = np.zeros((3, 4), np.float32)
+    right_cam_T_chess_ground_truth[0:3, 0:3] = cv2.Rodrigues(rvec_cam_right)[0]
+    right_cam_T_chess_ground_truth[0:3, 3] = tvec_cam_right.T
+
+    # Get point coordinates in homogeneous format
+    points_chessboard_homogeneous = np.zeros((4, dimensions[0] * dimensions[1]), np.float32)
+    points_chessboard_homogeneous[0:3, :] = points_chessboard.T
+    points_chessboard_homogeneous[3, :] = 1
+
+    # Compute residuals
+    # each residual is the Euclidean distance from the optimized 3D point to the ground truth 3D point.
+    residuals = np.zeros((dimensions[0] * dimensions[1]))
+
+    pts_right_cam_opt = np.matmul(right_cam_T_chess_opt, points_chessboard_homogeneous)
+    pts_right_cam_ground_truth = np.matmul(right_cam_T_chess_ground_truth, points_chessboard_homogeneous)
+
+    for i in range(dimensions[0] * dimensions[1]):
+        points_op = np.matmul(np.array(right_cam_T_chess_opt), points_chessboard_homogeneous[:, i].reshape((4, 1)))
+        points_real_right = np.matmul(right_cam_T_chess_ground_truth, points_chessboard_homogeneous[:, i].reshape((4, 1)))
+        residuals[i] = np.sqrt(
+            (points_op[0] - points_real_right[0]) ** 2 + (points_op[1] - points_real_right[1]) ** 2 + (
+                        points_op[2] - points_real_right[2]) ** 2)
+
+    # Draw projection of (optimized) 3D points
+    r_cam2tochess_vector, _ = cv2.Rodrigues(right_cam_T_chess_opt[0:3, 0:3])
+    # t_cam2tochess = np.zeros((3, 1))
+    t_cam2tochess = right_cam_T_chess_opt[0:3, 3]
+    imgpoints_right_optimize = cv2.projectPoints(pts_chessboard, r_cam2tochess_vector, t_cam2tochess, k_right,
+                                                 dist_right)
+
+    img1 = cv2.imread('./fotos_calibration/top_right_camera_4.jpg')
+    img = cv2.drawChessboardCorners(img1, (9, 6), imgpoints_right_optimize[0], True)
+    cv2.imshow('img', img)
+    cv2.waitKey(20)
+
+    return residuals
+
+
+def find_cam_chess_realpoints(fname, left_or_right):
     objpoints_left = []
     imgpoints_left = []
     objpoints_right = []
@@ -75,13 +138,13 @@ def find_cam_chess_realpoints(fname,left_or_right):
     # If found, add object points, image points (after refining them)
     if ret == True:
         if left_or_right == 0:
-            objpoints_left.append(objp)
+            objpoints_left.append(pts_chessboard)
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints_left.append(corners2)
             retval, rvec, tvec = cv2.solvePnP(objpoints_left[0], imgpoints_left[0], k_left,
-                                                        dist_left)  # calculating the rotation and translation vectors from left camera to chess board
+                                              dist_left)  # calculating the rotation and translation vectors from left camera to chess board
         elif left_or_right == 1:
-            objpoints_right.append(objp)
+            objpoints_right.append(pts_chessboard)
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints_right.append(corners2)
             retval, rvec, tvec = cv2.solvePnP(objpoints_right[0], imgpoints_right[0], k_right,
@@ -89,34 +152,37 @@ def find_cam_chess_realpoints(fname,left_or_right):
 
     return rvec, tvec
 
+
 def inv_tranformation_matrix(r, t):
     x_array = np.array(r[0])
     r_rot = []
     r_rot[0] = r[0].T * -1
     t_rot = np.matmul(r_rot, t[0][0:3])
-    r[0]=r[0].T
+    r[0] = r[0].T
     t[0][0:3] = t_rot
     return r, t
+
 
 # Chessboard dimensions
 dimensions = (6, 9)
 size_board = 100
 
 # K matrix and distortion coefficients from cameras
-k_left = np.array([[1149.369, 0.0, 471.693],[0.0, 1153.728, 396.955],[0.0, 0.0, 1.0]])
-dist_left = np.array([ -1.65512977e-01, -2.08184195e-01, -2.17490237e-03, -5.04628479e-04, 1.18772434e+00])
+k_left = np.array([[1149.369, 0.0, 471.693], [0.0, 1153.728, 396.955], [0.0, 0.0, 1.0]])
+dist_left = np.array([-1.65512977e-01, -2.08184195e-01, -2.17490237e-03, -5.04628479e-04, 1.18772434e+00])
 
-k_right = np.array([[1135.560, 0.0, 490.807],[0.0, 1136.240, 412.468],[0.0, 0.0, 1.0]])
-dist_right = np.array([ -2.06069540e-01, -1.27768958e-01, 2.22591520e-03, 1.60327811e-03, 2.08236968e+00])
+k_right = np.array([[1135.560, 0.0, 490.807], [0.0, 1136.240, 412.468], [0.0, 0.0, 1.0]])
+dist_right = np.array([-2.06069540e-01, -1.27768958e-01, 2.22591520e-03, 1.60327811e-03, 2.08236968e+00])
 
-# Creating chessboard points
-objp = generate_chessboard(100, (9, 6))
+
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-def main():
+# Creating chessboard points
+pts_chessboard = generate_chessboard(100, (9, 6))
 
+def main():
 
     # Arrays to store object points and image points from all the images.
     objpoints_left = []  # 3d point in real world space from left camera
@@ -140,13 +206,13 @@ def main():
             # If found, add object points, image points (after refining them)
             if ret == True:
                 if index == 0:
-                    objpoints_left.append(objp)
+                    objpoints_left.append(pts_chessboard)
 
                     corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
                     imgpoints_left.append(corners2)
 
                 elif index == 1:
-                    objpoints_right.append(objp)
+                    objpoints_right.append(pts_chessboard)
 
                     corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
                     imgpoints_right.append(corners2)
@@ -161,7 +227,12 @@ def main():
     params_initial = [1.0, 0.0, 0.0, 105.0, -200.0, 150.0]
 
     # print(objective_function(params_initial))
-    params_optimized = optimize.leastsq(objective_function, np.array(params_initial).reshape(1, 6), epsfcn=0.000001)
+    params_optimized = optimize.leastsq(func=objective_functionv2, x0=np.array(params_initial).reshape(1, 6),
+                                        epsfcn=0.000001, args=(pts_chessboard))
+
+
+    print('Optimization finished. Press any key to continue.')
+    cv2.waitKey(0)
     print('Vetor de erros:\n')
     print(objective_function(params_optimized[0]))
     print()
